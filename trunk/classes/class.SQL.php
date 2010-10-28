0354a5db39a97dc4f56032878e08a3c29660220c
eodivision <james@eodivision.com>
2010-10-28 20:31:57 +0100

    Added basic database class with MySQL extended class

---
+++ trunk/classes/class.SQL.php
@@ -0,0 +1,31 @@
+<?php
+/*
+	eoCMS © 2007 - 2010, a Content Management System
+	by James Mortemore
+	http://www.eocms.com
+	is licenced under a Creative Commons
+	Attribution-Share Alike 2.0 UK: England & Wales Licence.
+	Permissions beyond the scope of this licence 
+	may be available at http://creativecommons.org/licenses/by-sa/2.0/uk/.
+	Additional licence terms at http://eocms.com/licence.html
+*/
+abstract class SQL {
+	var $last_resource;
+	public abstract function connect($info);
+	public abstract function query($query);
+	public abstract function fetch_array($resource);
+	public abstract function fetch_assoc($resource);
+	public abstract function fetch_object($resource);
+	public abstract function fetch_row($resource);
+	public abstract function affected_rows();
+	public abstract function insert_id();
+	public abstract function num_rows($resource);
+	public function resource($resource) {
+		if(empty($resource))
+			return $this->last_resource;
+		else
+			return $resource;
+	}
+}
+require('database/'.DB_TYPE.'.php');
+?>
---
+++ trunk/classes/database/MySQL.php
@@ -0,0 +1,43 @@
+<?php
+/*
+	eoCMS © 2007 - 2010, a Content Management System
+	by James Mortemore
+	http://www.eocms.com
+	is licenced under a Creative Commons
+	Attribution-Share Alike 2.0 UK: England & Wales Licence.
+	Permissions beyond the scope of this licence 
+	may be available at http://creativecommons.org/licenses/by-sa/2.0/uk/.
+	Additional licence terms at http://eocms.com/licence.html
+*/
+class MySQL extends SQL {
+	public function connect($info) {
+		mysql_connect($info['host'], $info['user'], $info['password']);
+		mysql_select_db($info['database']);
+	}
+	public function query($query) {
+		$this->last_resource = mysql_query($query);
+		return $this->last_resoruce;
+	}
+	public function fetch_array($resource = '') {
+		return mysql_fetch_array(self::resource($resource));
+	}
+	public function fetch_assoc($resource = '') {
+		return mysql_fetch_assoc(self::resource($resource));
+	}
+	public function fetch_object($resource = '') {
+		return mysql_fetch_object(self::resource($resource));
+	}
+	public function fetch_row($resource = '') {
+		return mysql_fetch_object(self::resource($resource));
+	}
+	public function affected_rows() {
+		return mysql_affected_rows();
+	}
+	public function insert_id() {
+		return mysql_insert_id();
+	}
+	public function num_rows($resource = '') {
+		return mysql_num_rows(self::resource($resource));
+	}
+}
+?>
