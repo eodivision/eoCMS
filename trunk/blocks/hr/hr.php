8d97ee79e45563cfa3187b14240839b9a9293ac1
eodivision <james@eodivision.com>
2011-03-14 20:55:48 +0000

    Added hr and image blocks

---
+++ trunk/blocks/hr/hr.php
@@ -0,0 +1,18 @@
+<?php
+/*
+	eoCMS © 2007 - 2010, a Content Management System
+	by James Mortemore
+	http://www.eocms.com
+	is licenced under a Creative Commons
+	Attribution-Share Alike 2.0 UK: England & Wales Licence.
+	Permissions beyond the scope of this licence 
+	may be available at http://creativecommons.org/licenses/by-sa/2.0/uk/.
+	Additional licence terms at http://eocms.com/licence.html
+*/
+
+class hr {
+	function html() {
+		return '<hr />';
+	}
+}
+?>
